f111dd0033e407e83c0e7eb98394a14f1f4dbd31
MemetBadut <pasekm06@gmail.com>
2025-12-17 16:22:20 +0800

    nambah optimasi untuk seeding 500K data (23 menit cuma untuk 500K data? KOCAK!)

--- database/seeders/DummyVoterSeeder.php
+++ database/seeders/DummyVoterSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\DataVoters;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DummyVoterSeeder extends Seeder
@@ -14,10 +15,22 @@ class DummyVoterSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i = 0; $i < 500000; $i++) {
-            DataVoters::create([
+        $data = [];
+        $totalVoters = 500000;
+        $chunkSize = 50000;
+
+        for ($i = 0; $i < $totalVoters; $i++) {
+            // DataVoters::create([
+            //     'voter_palsu' => Str::uuid(),
+            // ]);
+            $data[] = [
                 'voter_palsu' => Str::uuid(),
-            ]);
+            ];
+
+            if ($i % $chunkSize === 0) {
+                DB::table('data_voters')->insert($data);
+                $data = []; // Kosongkan lagi array-nya biar hemat RAM
+            }
         }
     }
 }
