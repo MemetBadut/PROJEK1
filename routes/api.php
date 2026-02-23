ec11c9f18c169ac75c79b5d14b4bf45ee4cca71b
MemetBadut <pasekm06@gmail.com>
2026-02-23 16:21:29 +0800

    fix:delete use App\Http\Resources\BookResource;

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\RatingController;
-use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
