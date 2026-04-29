375be0f3d480b22b658bbba1dcc0f7b280424a1a
MemetBadut <pasekm06@gmail.com>
2026-04-29 14:05:16 +0800

    fix: adding missing fillable row

--- app/Models/DataVoters.php
+++ app/Models/DataVoters.php
@@ -10,12 +10,21 @@ class DataVoters extends Model
         'produk_buku_id',
         'total_voters',
         'avg_rating',
-        'avg_7_days'
+        'total_rating_sum',
+        'avg_prev_7_days',
+        'avg_30_days',
+        'voters_30_days',
+        'recent_popularity_score',
+        'avg_7_days',
+        'trend_7_days',
+        'trend_direction',
     ];
+
 
     protected $table = 'data_voters';
 
-    public function ratingBuku(){
+    public function ratingBuku()
+    {
         return $this->belongsTo(RatingUser::class, 'rating_user_id', 'id');
     }
 }
