b07c0dacc924e30a6d82ed1d0b40cb3e88fe3d07
MemetBadut <pasekm06@gmail.com>
2026-05-04 11:57:44 +0800

    feat: add request for delete rating user

---
+++ app/Http/Requests/DeleteRatingRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Override;
+
+class DeleteRatingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('rating'),
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|exists:rating_users,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id.exists' => 'Rating tidak ditemukan.',
+            'id.required' => 'Rating wajib diisi.',
+        ];
+    }
+}
