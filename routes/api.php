7e80e46788c821e1597a9113cc6cbbfc19b55443
MemetBadut <pasekm06@gmail.com>
2026-06-04 15:37:19 +0800

    fix: middleware auth for book, rating, and author only()

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,9 @@ Route::post('/login', [AuthController::class, 'loginApi']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logoutApi']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/login', [AuthController::class, 'loginApi']);
-    Route::post('/logout', [AuthController::class, 'logoutApi']);
-
-    Route::apiResource('books', BookController::class)->only('store', 'update', 'delete');
-    Route::apiResource('ratings', RatingController::class)->only('store', 'update', 'delete');
-    Route::apiResource('authors', AuthorController::class)->only('store', 'update', 'delete');
+    Route::apiResource('books', BookController::class)->only('store', 'update', 'destroy');
+    Route::apiResource('ratings', RatingController::class)->only('store', 'update', 'destroy');
+    Route::apiResource('authors', AuthorController::class)->only('store', 'update', 'destroy');
 });
 
 Route::apiResource('books', BookController::class)->only('index', 'show');
