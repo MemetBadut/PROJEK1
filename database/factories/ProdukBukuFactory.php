5b9b26f0d30f9cba17d0e42a50692c0e8c0ad792
MemetBadut <pasekm06@gmail.com>
2026-06-12 11:39:06 +0800

    fix: generate unique slug for each book

--- database/factories/ProdukBukuFactory.php
+++ database/factories/ProdukBukuFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\PenulisBuku;
 use App\Models\PublisherBuku;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 
 /**
@@ -17,38 +18,38 @@ class ProdukBukuFactory extends Factory
      *
      * @return array<string, mixed>
      */
-public function definition(): array
-{
-    static $penulisIds;
-    static $publisherIds;
-    $title = fake()->sentence(3);
-    $sinopsis = fake()->sentence(3);
+    public function definition(): array
+    {
+        static $penulisIds;
+        static $publisherIds;
+        $title = fake()->sentence(3);
+        $sinopsis = fake()->sentence(3);
+        $uniqueSuffix = fake()->unique()->numberBetween(100000, 999999);
 
-    $penulisIds ??= PenulisBuku::pluck('id')->toArray();
-    $publisherIds ??= PublisherBuku::pluck('id')->toArray();
+        $penulisIds ??= PenulisBuku::pluck('id')->toArray();
+        $publisherIds ??= PublisherBuku::pluck('id')->toArray();
 
-    $status = fake()->randomElement(
-        array_merge(
-            array_fill(0, 80, 'tersedia'),
-            array_fill(0, 15, 'tersimpan'),
-            array_fill(0, 5, 'dipinjam'),
-        )
-    );
+        $status = fake()->randomElement(
+            array_merge(
+                array_fill(0, 80, 'tersedia'),
+                array_fill(0, 15, 'tersimpan'),
+                array_fill(0, 5, 'dipinjam'),
+            )
+        );
 
-    $createdAt = fake()->dateTimeBetween('-3 years', 'now');
+        $createdAt = fake()->dateTimeBetween('-3 years', 'now');
 
-    return [
-        'nama_buku' => $title,
-        'penulis_buku_id' => fake()->randomElement($penulisIds),
-        'isbn' => fake()->isbn10(),
-        'publisher_id' => fake()->randomElement($publisherIds),
-        'status_buku' => $status,
-        'rating_enabled' => $status === 'tersedia',
-        'slug' => str($title)->slug(),
-        'sinopsis' => $sinopsis,
-        'created_at' => $createdAt,
-        'updated_at' => fake()->dateTimeBetween($createdAt, 'now'),
-    ];
-}
-
+        return [
+            'nama_buku' => $title,
+            'penulis_buku_id' => fake()->randomElement($penulisIds),
+            'isbn' => fake()->isbn10(),
+            'publisher_id' => fake()->randomElement($publisherIds),
+            'status_buku' => $status,
+            'rating_enabled' => $status === 'tersedia',
+            'slug' => Str::slug($title . '-' . $uniqueSuffix),
+            'sinopsis' => $sinopsis,
+            'created_at' => $createdAt,
+            'updated_at' => fake()->dateTimeBetween($createdAt, 'now'),
+        ];
+    }
 }
